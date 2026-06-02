c625f70808bf90cef3d3f1f8f1cff49aaa28ca04
Ramil <zarram89@gmail.com>
2026-06-02 16:09:07 +0500

    6.8. Обновление века (часть 1)

--- src/view/film-details-form-template.js
+++ src/view/film-details-form-template.js
@@ -1,6 +1,6 @@
 import {EMOTIONS} from '../const.js';
 
-const createEmotionItem = (emotionItem) =>
+const createEmotionItem = (emotionItem, checkedEmotionValue) =>
   `
     <input
       class="film-details__emoji-item visually-hidden"
@@ -8,8 +8,13 @@ const createEmotionItem = (emotionItem) =>
       type="radio"
       id="emoji-${emotionItem}"
       value="${emotionItem}"
+      ${emotionItem === checkedEmotionValue ? 'checked' : ''}
     >
-    <label class="film-details__emoji-label" for="emoji-${emotionItem}">
+    <label
+      class="film-details__emoji-label"
+      for="emoji-${emotionItem}"
+      data-emotion-type=${emotionItem}
+    >
       <img
         src="./images/emoji/${emotionItem}.png"
         width="30"
@@ -19,17 +24,23 @@ const createEmotionItem = (emotionItem) =>
     </label>
   `;
 
-export const createFilmDetailsFormTemplate = () =>
+export const createFilmDetailsFormTemplate = (checkedEmotion, comment) =>
   `
     <form class="film-details__new-comment"  action="" method="get">
-      <div class="film-details__add-emoji-label"></div>
+      <div class="film-details__add-emoji-label">
+      ${(checkedEmotion) ? `<img src="images/emoji/${checkedEmotion}.png" width="55" height="55" alt="emoji-${checkedEmotion}">` : ''}
+      </div>
 
       <label class="film-details__comment-label">
-        <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+        <textarea
+          class="film-details__comment-input"
+          placeholder="Select reaction below and write comment here"
+          name="comment"
+        >${(comment) ? comment : ''}</textarea>
       </label>
 
       <div class="film-details__emoji-list">
-        ${EMOTIONS.map(createEmotionItem).join('')}
+        ${EMOTIONS.map((emotion) => createEmotionItem(emotion, checkedEmotion)).join('')}
       </div>
     </form>
   `;
