0685b74ae1b6ea82ea277a20fab913653113c0a2
Ramil <zarram89@gmail.com>
2026-06-02 16:25:18 +0500

    6.9. Обновление века (часть 2)

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,28 +1,32 @@
-const formatStringToDateWithTime = (date) =>
-  new Date(date).toLocaleString('en-GB');
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-const formatStringToDate = (date) =>
-  new Date(date).toLocaleString('en-GB', {day: '2-digit', month: 'long', year: 'numeric'});
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
-const formatStringToYear = (date) =>
-  new Date(date).getFullYear();
-
-const formatMinutesToTime = (minutes) => {
-  const MINUTES_PER_HOUR = 60;
-
-  return (minutes < MINUTES_PER_HOUR)
-    ? `${minutes}m`
-    : `${Math.floor(minutes / MINUTES_PER_HOUR)}h ${minutes % MINUTES_PER_HOUR}m`;
+const humanizeDate = (date) => {
+  const timeDiff = dayjs(date).diff(dayjs());
+  return dayjs.duration(timeDiff).humanize(true);
 };
 
+const formatStringToDate = (date) =>
+  dayjs(date).format('DD MMMM YYYY');
+
+const formatStringToYear = (date) =>
+  dayjs(date).format('YYYY');
+
+const formatMinutesToTime = (minutes) =>
+  dayjs.duration(minutes, 'minutes').format('H[h] mm[m]');
+
 const sortFilmsByDate = (filmA, filmB) =>
-  new Date(filmB.filmInfo.release.date) - new Date(filmA.filmInfo.release.date);
+  dayjs(filmB.filmInfo.release.date).diff(dayjs(filmA.filmInfo.release.date));
 
 const sortFilmsByRating = (filmA, filmB) =>
   filmB.filmInfo.totalRating - filmA.filmInfo.totalRating;
 
 export {
-  formatStringToDateWithTime,
+  humanizeDate,
   formatStringToDate,
   formatStringToYear,
   formatMinutesToTime,
